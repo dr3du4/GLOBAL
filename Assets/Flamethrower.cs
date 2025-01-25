12331d64657f1272122db15c787dc07461959ec8
Jan Krzyszkowski <jan.krzyszkowski2@gmail.com>
2025-01-25 10:10:19 +0100

    flame

---
+++ Assets/Flamethrower.cs
@@ -0,0 +1,74 @@
+using System;
+using UnityEngine;
+
+public class Flamethrower : MonoBehaviour
+{
+    public ParticleSystem fireEffect; // Referencja do systemu cząsteczek
+    public float damage = 10f;       // Obrażenia
+    public float range = 5f;         // Zasięg miotacza ognia
+    public LayerMask damageLayer;    // Warstwa obiektów, które mogą być trafione
+
+    private bool isFiring = false;
+
+    void Update()
+    {
+        // Włącz/wyłącz ogień
+        if (Input.GetMouseButton(0)) // LPM do aktywacji
+        {
+            Debug.Log("Fire");
+            StartFiring();
+        }
+        else
+        {
+            StopFiring();
+        }
+
+        // Zadaj obrażenia w trakcie strzelania
+        if (isFiring)
+        {
+            ApplyDamage();
+        }
+    }
+
+    void StartFiring()
+    {
+        if (!isFiring)
+        {
+            fireEffect.Play(); // Uruchom cząsteczki
+            isFiring = true;
+        }
+    }
+
+    void StopFiring()
+    {
+        if (isFiring)
+        {
+            fireEffect.Stop(); // Zatrzymaj cząsteczki
+            isFiring = false;
+        }
+    }
+
+    void ApplyDamage()
+    {
+        // Znajdź wszystkie obiekty w zasięgu
+        Collider[] hitColliders =
+            Physics.OverlapSphere(transform.position + transform.forward * range / 2, range, damageLayer);
+
+        foreach (var hitCollider in hitColliders)
+        {
+            Debug.Log("Trafiono obiekt: " + hitCollider.name); // Wyświetla nazwę trafionego obiektu
+
+            // Zadaj obrażenia, jeśli obiekt ma komponent "Health"
+            var health = hitCollider.GetComponent<Health>();
+            if (health != null)
+            {
+                health.TakeDamage(damage * Time.deltaTime); // Obrażenia w czasie
+                Debug.Log("Zadano obrażenia: " + damage * Time.deltaTime);
+            }
+            else
+            {
+                Debug.Log("Obiekt " + hitCollider.name + " nie ma komponentu Health!");
+            }
+        }
+    }
+}
